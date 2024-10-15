5ea5bdc4513372cb16aa959a4ada1fb0160892f3
Gabriela <146675101+gabrielajamhour@users.noreply.github.com>
2024-10-15 12:44:25 +0200

    Add files via upload

---
+++ practica1/Square.java
@@ -0,0 +1,16 @@
+package practica1;
+
+
+/**
+ * class Square.
+ * 
+ * @author LTP
+ * @version 2020-21
+ */
+
+public class Square extends Rectangle {
+
+    public Square(double x, double y, double s) {
+        super(x, y, s, s);
+    }
+}
